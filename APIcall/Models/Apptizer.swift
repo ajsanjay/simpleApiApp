24200dd64c8f59530092cf40ec37e165055edb74
Jayasabeen <aj.sabeen@gmail.com>
2023-12-30 13:33:34 +0530

    Orders UI

--- APIcall/Models/Apptizer.swift
+++ APIcall/Models/Apptizer.swift
@@ -25,4 +25,8 @@ struct requestKey: Codable {
 struct MockData {
     static let sampleApptizer = Apptizer(calories: 999, protein: 99, id: 001, carbs: 99, price: 99.9, description: "This description is yummy", name: "Test iTem", imageURL: "")
     static let sampleData = [sampleApptizer, sampleApptizer, sampleApptizer, sampleApptizer]
+    static let orderItem1 = Apptizer(calories: 999, protein: 99, id: 001, carbs: 99, price: 99.9, description: "This description is yummy 11", name: "Test iTem order 1", imageURL: "")
+    static let orderItem2 = Apptizer(calories: 999, protein: 99, id: 001, carbs: 99, price: 99.9, description: "This description is yummy 22", name: "Test iTem order 2", imageURL: "")
+    static let orderItem3 = Apptizer(calories: 999, protein: 99, id: 001, carbs: 99, price: 99.9, description: "This description is yummy 33", name: "Test iTem order 3", imageURL: "")
+    static let orders = [orderItem1, orderItem2, orderItem3]
 }
--- APIcall/Screens/Orders/Orders.swift
+++ APIcall/Screens/Orders/Orders.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct Orders: View {
+    
+    @State private var orderItems = MockData.orders
+    
     var body: some View {
         NavigationView {
-            Text("Orders Loading ...")
-                .navigationTitle("🧳 My Orders")
+            VStack {
+                List {
+                    ForEach(orderItems) { appetizer in
+                        ApptizerListCell(apptizer: appetizer)
+                    }
+                    .onDelete(perform: deletItems)
+                }
+                .listStyle(PlainListStyle())
+                Button {
+                    print("Order Placed")
+                } label: {
+                    CartButton(titele: "Test")
+                }
+                .padding(.bottom, 20)
+            }
+            .navigationTitle("🧳 My Orders")
         }
     }
+    
+    func deletItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
+    }
+    
 }
 
 #Preview {
